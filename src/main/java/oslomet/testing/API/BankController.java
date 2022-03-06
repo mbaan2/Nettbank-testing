685c3adcf22d4b91d86d5ff234778e06eac0c1a4
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2022-03-07 00:52:16 +0100

    Small change

--- src/main/java/oslomet/testing/API/BankController.java
+++ src/main/java/oslomet/testing/API/BankController.java
@@ -1,10 +1,7 @@
 package oslomet.testing.API;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Models.Konto;
 import oslomet.testing.Models.Kunde;
@@ -69,7 +66,7 @@ public class BankController {
     }
 
     @GetMapping("/utforBetaling")
-    public List<Transaksjon> utforBetaling(Integer txID) {
+    public List<Transaksjon> utforBetaling(int txID) {
         String personnummer = sjekk.loggetInn();
         if (personnummer!=null) {
             if (repository.utforBetaling(txID).equals("OK")) {
